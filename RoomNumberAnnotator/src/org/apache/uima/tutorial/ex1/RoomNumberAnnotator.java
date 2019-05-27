406447bc2e188596f5e63f557f8135541cd1573c
Isabel Lally <43449214+ilally93@users.noreply.github.com>
2019-05-27 11:34:42 -0400

    add files via upload

---
+++ RoomNumberAnnotator/src/org/apache/uima/tutorial/ex1/RoomNumberAnnotator.java
@@ -0,0 +1,15 @@
+package org.apache.uima.tutorial.ex1;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+
+public class RoomNumberAnnotator extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void process(JCas aJCas) throws AnalysisEngineProcessException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
